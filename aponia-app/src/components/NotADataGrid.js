58d0287debeff4d1a30a19bb5c2ad117c70bc08d
Jesus M <warloy989@gmail.com>
2023-02-11 02:10:21 -0400

    NotDataGrid works

--- aponia-app/src/components/NotADataGrid.js
+++ aponia-app/src/components/NotADataGrid.js
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 export default function BasicTable({ rows = [], columns = [], loading = false }) {
 
@@ -27,62 +28,77 @@ export default function BasicTable({ rows = [], columns = [], loading = false })
   }, [rows])
 
   return (
-    <TableContainer component={Paper} elevation={0} >
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead sx={{ minWidth: '100%' }}>
-          <TableRow>
-            {columns?.map((column, key) => (
-              <TableCell
-                key={key}
-                align='center'
-              >
-                {column}
-              </TableCell>
-            )
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody sx={{ minHeight: 650 }}>
-          {auxRows?.length > 0 && auxRows?.map((row, index) => (
-            <TableRow
-              key={index}
-            >
-              {row?.map((item, key) => (
+      <TableContainer sx={{ mb:2 }} component={Paper} elevation={0}>
+        <Table sx={{ minWidth: 650, maxHeight: 550 }} aria-label="simple table">
+          <TableHead sx={{ minWidth: '100%' }}>
+            <TableRow>
+              {columns?.map((column, key) => (
                 <TableCell
                   key={key}
                   align='center'
-                  sx={{
-                    minHeight: 10,
-                    maxHeight: 25
-                  }}
                 >
-                  {item}
+                  {column}
                 </TableCell>
-              ))}
+              )
+              )}
             </TableRow>
-          ))}
-        </TableBody>
+          </TableHead>
+          <TableBody sx={{ minHeight: 650, maxHeight: 550 }}>
+            {auxRows?.length > 0 && auxRows?.map((row, index) => (
+              <TableRow
+                key={index}
+              >
+                {row?.map((item, key) => (
+                  <TableCell
+                    key={key}
+                    align='center'
+                    sx={{
+                      minHeight: 10,
+                      maxHeight: 25
+                    }}
+                  >
+                    {item}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
 
-      </Table>
-      {loading &&
-        <div
-          style={{
-            flex: 1,
-            display: 'flex',
-            minWidth: '100%',
-            minHeight: '100%',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <img
-            height={256}
-            width={256}
-            src='https://i.stack.imgur.com/ATB3o.gif'
-            alt='gif'
-          />
-        </div>
+        </Table>
+        {loading &&
+          <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              minWidth: '100%',
+              minHeight: '100%',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <img
+              height={256}
+              width={256}
+              src='https://i.stack.imgur.com/ATB3o.gif'
+              alt='gif'
+            />
+          </div>
         }
-    </TableContainer>
+        {auxRows?.length === 0 && !loading &&
+            <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              minWidth: '100%',
+              minHeight: '100%',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography sx={{my:5}}>
+              No se encontraron datos
+            </Typography>
+          </div>}
+      </TableContainer>
   );
 }
--- aponia-app/src/pages/Reports.js
+++ aponia-app/src/pages/Reports.js
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import { AppBar, 
-    Toolbar,
-    Typography,
-    Paper,
-    Grid,
-    TextField,
-    Tooltip,
-    FormControl,
-    IconButton,
-    InputLabel,
-    NativeSelect,
-    InputAdornment,
-    Box
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Paper,
+  Grid,
+  TextField,
+  Tooltip,
+  FormControl,
+  IconButton,
+  InputLabel,
+  NativeSelect,
+  InputAdornment,
+  Box,
+  Button
 } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import Toaster from '../hooks/useToast';
@@ -24,18 +26,25 @@ import BasicTable from '../components/NotADataGrid'
 import SearchIcon from '@mui/icons-material/Search';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
-const columns = ["Código de sucursal","Nombre de sucursal","Código de Medicina","Nombre de laboratorio","Nombre","Presentación","Cantidad en inventario"];
+const columns = ["Código de sucursal", "Nombre de sucursal", "Código de Medicina", "Nombre de laboratorio", "Nombre de medicina", "Presentación", "Cantidad en inventario"];
 
 export default function Reports() {
-  const {showErrorToast} = Toaster();
+  const { showErrorToast } = Toaster();
   const [searchParams, setSearchParams] = React.useState('');
   const [searchFilter, setSearchFilter] = React.useState('');
   const [officeList, setOfficeList] = React.useState([])
   const [collection, setCollection] = React.useState([]);
   const location = useLocation();
-	const { startLoading, stopLoading, isLoading } = useLoading()
+  const { startLoading, stopLoading, isLoading } = useLoading()
   const [currentPage, setCurrentPage] = React.useState(0)
+  const [totalPages, setTotalPages] = React.useState(0)
+
+  const [totalRows, setTotalRows] = React.useState(0)
+  const [currentRows, setCurrentRows] = React.useState(0)
+  const [currentRowEnd, setCurrentRowEnd] = React.useState(0)
 
   const [isLoadingOffices, setIsLoadingOffices] = React.useState(false)
 
@@ -68,19 +77,22 @@ export default function Reports() {
 
       const ActiveSID = JSON.parse(localStorage.getItem('@user'))?.SID
       const ActiveOffice = await getOffice(ActiveSID)
-      
+
       const body = {
         pag: currentPage || 0,
         size: size || 10,
         oc: type === '0' ? searchParams : ActiveOffice?.code,
-        mc: searchFilter || '', 
+        mc: searchFilter || '',
       }
 
-      console.log(body)
-      
       const registerCount = await getInventoryQty({ oc: body.oc, mc: body.mc, }) //Amount of registers after applying the filter
-      const pageCount = Math.round(Number(registerCount/size)) //Amount of pages after applying the filter
-      console.log(`Regs: ${registerCount}, Pages: ${pageCount}`)
+      const pageCount = Math.round(Number(registerCount / size)) //Amount of pages after applying the filter
+      const currentRowStart = ((currentPage * size) + 1)
+      const cre = Math.min(((currentPage + 1) * size), registerCount)
+      setCurrentRows(currentRowStart)
+      setCurrentRowEnd(cre)
+      setTotalRows(registerCount)
+      setTotalPages(pageCount)
 
       const resp = await getInventoryList(body)
 
@@ -97,7 +109,7 @@ export default function Reports() {
           quantity: item?.quantity
         })
       })
-      
+
       setCollection(aux)
 
       stopLoading()
@@ -108,26 +120,34 @@ export default function Reports() {
     }
   }
 
-  React.useEffect(()=>{
-    if(officeList?.length === 0) {
+  React.useEffect(() => {
+    if (officeList?.length === 0) {
       getOfficeList()
     }
     searchData()
-  },[officeList, currentPage, searchParams, searchFilter])
+  }, [officeList, currentPage, searchParams, searchFilter])
 
 
   const handleSearchParams = (event) => {
     setCollection([])
+    setCurrentPage(0)
+    setCurrentRows(0)
+    setCurrentRowEnd(0)
+    setTotalRows(0)
     let value = event.target.value
-    value === null ? setSearchParams('') : 
-    setSearchParams(value)
+    value === null ? setSearchParams('') :
+      setSearchParams(value)
   }
 
   const handleSearchFilter = (event) => {
     setCollection([])
+    setCurrentPage(0)
+    setCurrentRows(0)
+    setCurrentRowEnd(0)
+    setTotalRows(0)
     let value = event.target.value
-    value === null ? setSearchParams('') : 
-    setSearchFilter(value)
+    value === null ? setSearchParams('') :
+      setSearchFilter(value)
   }
 
   const handleCleanUp = () => {
@@ -135,9 +155,9 @@ export default function Reports() {
     setSearchFilter('')
   }
 
-    return (
+  return (
     <Paper elevation='0' sx={{ maxWidth: 1366, margin: 'auto', overflow: 'hidden' }}>
-      <Typography sx={{ mt: 5, mb:0, mx: 2 }} color="text.secondary" align="center">
+      <Typography sx={{ mt: 5, mb: 0, mx: 2 }} color="text.secondary" align="center">
         Reportes
       </Typography>
       <AppBar
@@ -146,100 +166,161 @@ export default function Reports() {
         elevation={0}
         sx={{ maxWidth: 1366, borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
       >
-        { location.pathname==='/admin/reports' ?
-        <>
-        <Toolbar>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12} md={4}>
-                <FormControl sx={{ m: 1, width:1, margin:'none' }}  variant="standard">
+        {location.pathname === '/admin/reports' ?
+          <>
+            <Toolbar>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item xs={12} md={4}>
+                  <FormControl sx={{ m: 1, width: 1, margin: 'none' }} variant="standard">
                     <InputLabel>Lista de Sucursales</InputLabel>
                     <NativeSelect
-                        labelId="demo-simple-select-label"
-                        id="reports-office-list"
-                        value={searchParams}
-                        label="Lista de Sucursales"
-                        onChange={handleSearchParams}
-                        endAdornment={
-                            <InputAdornment position="start">
-                                <QrCode2Icon />
-                            </InputAdornment>
-                        }
+                      labelId="demo-simple-select-label"
+                      id="reports-office-list"
+                      value={searchParams}
+                      label="Lista de Sucursales"
+                      onChange={handleSearchParams}
+                      endAdornment={
+                        <InputAdornment position="start">
+                          <QrCode2Icon />
+                        </InputAdornment>
+                      }
                     >
-                        <option value=""></option>
-                        {officeList.map(({SID, code}) => (
-                            <option key={SID} value={code}>{code}</option>
-                        ))}
+                      <option value=""></option>
+                      {officeList.map(({ SID, code }) => (
+                        <option key={SID} value={code}>{code}</option>
+                      ))}
                     </NativeSelect>
-                </FormControl>
-            </Grid>
-            <Grid item>
-                <SearchIcon color="inherit" sx={{ display: 'block' }} />
-            </Grid>
-            <Grid item xs>
-                <TextField
+                  </FormControl>
+                </Grid>
+                <Grid item>
+                  <SearchIcon color="inherit" sx={{ display: 'block' }} />
+                </Grid>
+                <Grid item xs>
+                  <TextField
                     fullWidth
                     placeholder="Buscar por código de medicina"
                     value={searchFilter}
                     onChange={handleSearchFilter}
                     autoFocus
-                    inputProps={{maxlength:50}}
+                    inputProps={{ maxlength: 50 }}
                     InputProps={{
-                    disableUnderline: true,
-                    sx: { fontSize: 'default' },
+                      disableUnderline: true,
+                      sx: { fontSize: 'default' },
                     }}
                     variant="standard"
-                />
-            </Grid>
-            <Grid item>
-                <Tooltip title="Reload">
-                    <IconButton onClick={handleCleanUp}>
-                        <RefreshIcon color="inherit" sx={{ display: 'block' }} />
-                    </IconButton>
-                </Tooltip>
-            </Grid>
-          </Grid>
-         </Toolbar>
-        </>
-        :<>
-            <Toolbar>
-                <Grid container spacing={2} alignItems="center">
-                    <Grid item>
-                        <SearchIcon color="inherit" sx={{ display: 'block' }} />
-                    </Grid>
-                    <Grid item xs>
-                        <TextField
-                            fullWidth
-                            placeholder="Buscar por código de medicina"
-                            value={searchFilter}
-                            onChange={handleSearchFilter}
-                            autoFocus
-                            inputProps={{maxlength:50}}
-                            InputProps={{
-                            disableUnderline: true,
-                            sx: { fontSize: 'default' },
-                            }}
-                            variant="standard"
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Tooltip title="Reload">
-                            <IconButton onClick={handleCleanUp}>
-                                <RefreshIcon color="inherit" sx={{ display: 'block' }} />
-                            </IconButton>
-                        </Tooltip>
-                    </Grid>
+                  />
                 </Grid>
+                <Grid item>
+                  <Tooltip title="Reload">
+                    <IconButton onClick={handleCleanUp}>
+                      <RefreshIcon color="inherit" sx={{ display: 'block' }} />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+              </Grid>
             </Toolbar>
-        </>}
+          </>
+          : <>
+            <Toolbar>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item>
+                  <SearchIcon color="inherit" sx={{ display: 'block' }} />
+                </Grid>
+                <Grid item xs>
+                  <TextField
+                    fullWidth
+                    placeholder="Buscar por código de medicina"
+                    value={searchFilter}
+                    onChange={handleSearchFilter}
+                    autoFocus
+                    inputProps={{ maxlength: 50 }}
+                    InputProps={{
+                      disableUnderline: true,
+                      sx: { fontSize: 'default' },
+                    }}
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item>
+                  <Tooltip title="Reload">
+                    <IconButton onClick={handleCleanUp}>
+                      <RefreshIcon color="inherit" sx={{ display: 'block' }} />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </>}
       </AppBar>
-      <Box sx={{ height: 650, width: '100%' }}>
+      <Box sx={{ width: '100%', alignItems: 'space-between' }}>
+        <div
+          style={{
+          }}
+        >
+          <BasicTable
+            loading={isLoading}
+            rows={collection}
+            columns={columns}
+          />
+        </div>
 
-      <BasicTable 
-        loading={isLoading}
-        rows={collection}
-        columns={columns}
-      />
-    </Box>
+
+        {totalRows > 0 &&
+          <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '100%'
+            }}
+          >
+            <Typography variant="caption" sx={{ ml: 3, mb: 2 }}>
+              Mostrando resultados {currentRows}-{currentRowEnd} de {totalRows}
+            </Typography>
+
+            <div
+              style={{
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                width: '100%',
+                padding: 2,
+              }}
+            >
+              <IconButton
+                sx={{ mx: 2, mb: 2 }}
+                color="primary"
+                disabled={currentPage === 0}
+                onClick={() => {
+                  if (currentPage > 0) {
+                    setCurrentPage(currentPage - 1)
+                  }
+                }}
+              >
+                <NavigateBeforeIcon />
+              </IconButton>
+              <Typography sx={{ mb: 2 }} >
+                Página {currentPage + 1}/{totalPages + 1}
+              </Typography>
+              <IconButton
+                color="primary"
+                sx={{ mx: 2, mb: 2 }}
+                disabled={currentPage === totalPages}
+                onClick={() => {
+                  if (currentPage < totalPages) {
+                    setCurrentPage(currentPage + 1)
+                  }
+                }}
+              >
+                <NavigateNextIcon />
+              </IconButton>
+            </div>
+          </div>
+        }
+
+      </Box>
     </Paper>
   );
 }
